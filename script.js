a70aca3335286575a6b452cdc1e51724b1e2d82b
Aman Rajput <89723728+amanrajput12@users.noreply.github.com>
2025-03-16 14:44:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
 // Complete the js code
-function Car(make, model) {}
+class Car{
+ constructor(make,model){
+ this.make=make;
+	 this.model=model;
+ }
+	getMakeModel(){
+    retrun 'Make is '+this.make +" and model is "+this.model;
+}
+}
 
-function SportsCar(make, model, topSpeed) {}
+class SportsCar extends Car{
+   constructor(make,model,topspeed){
+     super(make,model);
+	   this.topspeed=topspeed;
+   }
+	getTopSpeed(){
+ return "top speed of car is "+this.topspeed;
+	}
+}
+
+
+// Create a new SportsCar instance
+const car = new SportsCar("Ferrari", "Testarossa", 200);
+
+// Call getMakeModel() to get the make and model of the car
+console.log(car.getMakeModel()); // Output: Ferrari Testarossa
+
+// Call getTopSpeed() to get the top speed of the sports car
+console.log(car.getTopSpeed()); // Output: 200
 
 // Do not change the code below
 window.Car = Car;
